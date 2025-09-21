55507293f7aadb3aa89fe85dde2439f747bb4a45
Harshit Sharma <sharma.harshit0059@gmail.com>
2025-09-21 23:33:52 +0530

    Comment out initial JavaScript code examples for clarity

--- JavaScript - The Complete Guide (by Jonas Schmedtmann)/Fundamentals of JavaScript/Tutorials/Tutorial - 1/script.js
+++ JavaScript - The Complete Guide (by Jonas Schmedtmann)/Fundamentals of JavaScript/Tutorials/Tutorial - 1/script.js
@@ -1,10 +1,38 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Matilda";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Matilda";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// console.log(true);
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+birthYear = 1990;
