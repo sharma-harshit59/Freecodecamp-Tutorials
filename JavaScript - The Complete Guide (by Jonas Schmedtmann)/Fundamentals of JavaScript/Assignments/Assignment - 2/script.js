e13e229f1c6ad00f7e47578d0c76d3c59848365a
Harshit Sharma <sharma.harshit0059@gmail.com>
2025-09-23 13:00:16 +0530

    Add HTML and JavaScript files for Assignment 2 and Assignment 3

---
+++ JavaScript - The Complete Guide (by Jonas Schmedtmann)/Fundamentals of JavaScript/Assignments/Assignment - 2/script.js
@@ -0,0 +1,11 @@
+let country = "India";
+let continent = "Asia";
+let population = 1400000000;
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ JavaScript - The Complete Guide (by Jonas Schmedtmann)/Fundamentals of JavaScript/Assignments/Assignment - 3/script.js
@@ -0,0 +1,15 @@
+const country = "India";
+const continent = "Asia";
+let population = 1400000000;
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "Hindi";
+
+isIsland = true;
